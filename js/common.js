5f3633deeb3a51922f81484b9eebf305ec4c0afb
流星的泪痕 <61955365@qq.com>
2015-01-04 22:15:20 +0800

    add detail

--- js/common.js
+++ js/common.js
@@ -26,6 +26,58 @@ function addFavorite() {
 /* @end **/
 
 /**
+* @name     :
+* @author   :Nice
+* @type     :基类
+* @dependent:
+*/
+function tabBar(){
+    var nav=$("#bootyNav");
+    var content=$("#bootyContent");
+    var list=content.children(".list");
+    var a=nav.children("a");
+
+    GLOBAL.namespace("TABBAR");
+    GLOBAL.TABBAR.index=0;
+
+
+    a.click(function(event) {
+
+        var i=GLOBAL.TABBAR.index=$(this).index();
+
+        $(list[i]).stop().animate({
+            left: -100+"%",
+            opacity:0
+        }, 1000)
+        $(list[i]).addClass('none');
+
+        // $(list[(i+1)]).removeClass('none');
+        // $(list[(i+1)]).addClass('block');
+        // $(list[(i+1)]).css('display', 'block');
+        // $(list[(i+1)]).stop().animate({
+            
+        //     opacity:1
+        // }, 1000)
+        // $(list[i+1])
+
+        // GLOBAL.TABBAR.index=i;
+
+
+        console.log(i);
+        console.log(list);
+        console.log(nav);
+        // i=
+    });
+
+
+
+}
+/* @end **/
+tabBar();
+
+
+
+/**
 * @name		:
 * @author	:Nice
 * @version	:
